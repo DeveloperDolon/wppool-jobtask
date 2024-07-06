36870902bbab52c89667d77da731e15ea392e528
Dolon Chandra Roy <dolonr718@gmail.com>
2024-07-06 14:20:28 +0600

    navbar added commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      primary: "#2042B6",
+    },
     fontFamily: {
       "dm-sans": ["DM Sans", "sans-serif"],
       "crimson-pro": ["Crimson Pro", "serif"],
