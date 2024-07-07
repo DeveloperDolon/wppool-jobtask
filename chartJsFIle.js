247028680ade1b39088dc4bd7585d884761ac6b2
Dolon Chandra Roy <dolonr718@gmail.com>
2024-07-07 15:52:23 +0600

    chart design complete commit

--- chartJsFIle.js
+++ chartJsFIle.js
@@ -1,20 +1,9 @@
 const dates = ["Feb", "Mar", "Apr", "May", "Jun", "Jul"];
 
-const wppool = [
-  265.02, 462.88, 163.41, 260.39, 159.28, 454.12, 555.57, 260.0, 361.0, 564.48,
-];
-
-const google = [
-  138.72, 237.26, 339.81, 135.37, 435.38, 125.91, 325.93, 234.59, 539.86, 247.5,
-];
-
-const microsoft = [
-  134.72, 337.26, 559.81, 235.37, 335.38, 525.91, 225.93, 734.59, 239.86, 247.5,
-];
-
-const twitter = [
-  434.72, 237.26, 579.81, 235.37, 535.38, 125.91, 325.93, 234.59, 439.86, 547.5,
-];
+const wppool = [265.02, 462.88, 163.41, 260.39, 159.28, 454.12];
+const google = [138.72, 237.26, 339.81, 135.37, 435.38, 125.91];
+const microsoft = [134.72, 337.26, 559.81, 235.37, 335.38, 525.91];
+const twitter = [434.72, 237.26, 579.81, 235.37, 535.38, 125.91];
 
 const data = {
   labels: dates,
@@ -22,22 +11,25 @@ const data = {
     {
       label: "WPPOOL",
       borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgb(255, 99, 132, 0.2)",
       data: wppool,
     },
     {
       label: "Google",
       borderColor: "rgb(0, 0, 255)",
+      backgroundColor: "rgba(0, 0, 255, 0.2)",
       data: google,
     },
     {
       label: "Microsoft",
       borderColor: "#7ca63a",
+      backgroundColor: "rgba(124, 166, 58, 0.2)",
       data: microsoft,
     },
     {
       label: "Twitter",
       borderColor: "#6F34A1",
-      fillStyle: "#6F34A1",
+      backgroundColor: "rgba(111, 52, 161, 0.2)",
       data: twitter,
     },
   ],
@@ -48,11 +40,39 @@ const config_line = {
   data: data,
   options: {
     responsive: true,
-    plugins: {
-      legend: {
-        display: true,
-        position: "bottom",
+    legend: {
+      display: true,
+      position: "bottom",
+      labels: {
+        boxWidth: 10,
+        padding: 10,
+        usePointStyle: true,
+        generateLabels: function (chart) {
+          return chart.data.datasets.map((dataset, i) => ({
+            text: dataset.label,
+            fillStyle: dataset.borderColor,
+            strokeStyle: dataset.borderColor,
+            hidden: !chart.isDatasetVisible(i),
+            lineCap: chart.config.options.elements.line.borderCapStyle,
+            lineDash: chart.config.options.elements.line.borderDash,
+            lineDashOffset: chart.config.options.elements.line.borderDashOffset,
+            lineJoin: chart.config.options.elements.line.borderJoinStyle,
+            lineWidth: chart.config.options.elements.line.borderWidth,
+            pointStyle: chart.config.data.datasets[i].pointStyle,
+            rotation: chart.config.data.datasets[i].rotation,
+          }));
+        },
       },
+    },
+  },
+  elements: {
+    line: {
+      tension: 0.4,
+    },
+  },
+  scales: {
+    y: {
+      beginAtZero: true,
     },
   },
 };
